c806131c63eb61f0b220a33a508d94f9c7beb082
Inna Patlakha <i.inna1903@gmail.com>
2023-07-24 00:11:44 +0300

    task1 done

---
+++ task1.js
@@ -0,0 +1,17 @@
+// Дан масив [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47] 
+// 1. Знайти суму та кількість позитивних елементів.
+
+let arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+let filteredArr = arr.filter (element => element > 0);
+console.log ('Відфільтрований масив тільки з позитивними елементами = ' + filteredArr);
+
+console.log('Кількість позитивних елементів масиву = ' + filteredArr.length);
+
+function arraySum(array){
+let sum = 0;
+for (let i = 0; i < array.length; i++) {
+  sum += array[i];
+}
+console.log('Сума позитивних елементів масиву = ' + sum);
+}
+arraySum(filteredArr);
